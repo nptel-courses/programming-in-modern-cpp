a6128d78813ccd6c2bffd458966c5c47166667ca
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-04 22:41:36 +0530

    WEEK 06: Assignment 01 - Problem 09         Consider the following code segment.
            #include<iostream>
            using namespace std;
            class base{
                int a;
                public:
                    base(int i) : a(i) {}
                    virtual void test(base *) { cout << a << endl; }
            };
            class derived : public base{
                int b;
                public:
                    derived(int i=0, int j=0) : base(i), b(j) { }
                    void test(derived *) { cout << b << endl; }
            };
            int main(){
                base *t1 = new derived(5,9);
                t1->test(new derived);  //Line-1
                return 0;
            }
    
            What will be the output?
    
            a) 0
            b) 5
            c) 9
            d) garbage
    
    ```cpp
    /**
     * option b). 5
     */
    #include <iostream>
    using namespace std;
    class base
    {
        int a;
    
    public:
        base(int i) : a(i) {}
        virtual void test(base *) { cout << a << endl; }
    };
    class derived : public base
    {
        int b;
    
    public:
        derived(int i = 0, int j = 0) : base(i), b(j) {}
        void test(derived *) { cout << b << endl; }
    };
    int main()
    {
        base *t1 = new derived(5, 9);
        t1->test(new derived);
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 06/P09/P09.cpp
@@ -0,0 +1,59 @@
+/*
+Consider the following code segment.
+
+#include<iostream>
+using namespace std;
+class base{
+    int a;
+    public:
+        base(int i) : a(i) {}
+        virtual void test(base *) { cout << a << endl; }
+};
+class derived : public base{
+    int b;
+    public:
+        derived(int i=0, int j=0) : base(i), b(j) { }
+        void test(derived *) { cout << b << endl; }
+};
+int main(){
+    base *t1 = new derived(5,9);
+    t1->test(new derived);  //Line-1
+    return 0;
+}
+
+
+What will be the output?
+
+a) 0  
+b) 5  
+c) 9  
+d) garbage
+*/
+
+/**
+ * option b). 5
+ */
+#include <iostream>
+using namespace std;
+class base
+{
+    int a;
+
+public:
+    base(int i) : a(i) {}
+    virtual void test(base *) { cout << a << endl; }
+};
+class derived : public base
+{
+    int b;
+
+public:
+    derived(int i = 0, int j = 0) : base(i), b(j) {}
+    void test(derived *) { cout << b << endl; }
+};
+int main()
+{
+    base *t1 = new derived(5, 9);
+    t1->test(new derived);
+    return 0;
+}
