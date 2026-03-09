f94801da0fa4f4d1b834a736058fae952e30bdac
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-09 11:15:45 +0530

    WEEK 02: Assignment 01 - Problem 01         Consider the following program.         #include <iostream>         using namespace std;
            int main(){
                const int a = 10;   // LINE-1
                a = 50;             // LINE-2
                cout << a;
                return 0;
            }
    
            What will be the output/error?
            a) 10
            b) 50
            c) Compilation Error at LINE-1:     const must be initialized
            d) Compilation Error at LINE-2:     assignment of read only variable ‘a’
    
    ```cpp
    #include <iostream>
    using namespace std;
    
    int main()
    {
        const int a = 10; // LINE-1
        a = 50;           // LINE-2
        cout << a;
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 02/P01/P01.cpp
@@ -0,0 +1,29 @@
+/*
+Consider the following program.
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int a = 10;   // LINE-1
+    a = 50;             // LINE-2
+    cout << a;
+    return 0;
+}
+
+What will be the output/error?
+a) 10
+b) 50
+c) Compilation Error at LINE-1:     const must be initialized
+d) Compilation Error at LINE-2:     assignment of read only variable ‘a’
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int a = 10; // LINE-1
+    a = 50;           // LINE-2
+    cout << a;
+    return 0;
+}
---
+++ sol/solution/WEEK 02/P01/README.md
@@ -0,0 +1,21 @@
+```cpp
+Consider the following program.
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int a = 10;   // LINE-1
+    a = 50;             // LINE-2
+    cout << a;
+    return 0;
+}
+
+What will be the output/error?
+a) 10
+b) 50
+c) Compilation Error at LINE-1:     const must be initialized
+d) Compilation Error at LINE-2:     assignment of read only variable ‘a’
+```
+```cpp
+d) Compilation Error at LINE-2:     assignment of read only variable ‘a’
+```
