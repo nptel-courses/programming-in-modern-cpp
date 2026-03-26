42066ec55ba8f6be9b4fbb366442f72559622508
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-26 15:08:17 +0530

    WEEK 03: Assignment 03 - Problem 01         Consider the following snippet:
            class Complex {
            private:
                int x, y;
            public:
                void setX(int x_) { x = x_; }
                void setY(int y_) { y = y_; }
                void print() { cout << x << ", " << y; }
                void incr() { x++; y++; }
                int incrX() { return x + 1; }
                int incrY() { return ++y; }
            };
    
            Identify the set of all methods that change the state of Complex class objects?
            a) setX(), setY(), print()
            b) setX(), setY(), incrX(), incrY()
            c) incr(), incrX(), incrY()
            d) setX(), setY(), incr(), incrY()
    
    ```cpp
    class Complex
    {
    private:
        int x, y;
    
    public:
        void setX(int x_) { x = x_; }
        void setY(int y_) { y = y_; }
        void print() { cout << x << ", " << y; }
        void incr()
        {
            x++;
            y++;
        }
        int incrX() { return x + 1; }
        int incrY() { return ++y; }
    };
    ```

---
+++ sol/solution/WEEK 03/P01/P01.cpp
@@ -0,0 +1,39 @@
+/*
+Consider the following snippet:
+
+class Complex {
+private:
+    int x, y;
+public:
+    void setX(int x_) { x = x_; }
+    void setY(int y_) { y = y_; }
+    void print() { cout << x << ", " << y; }
+    void incr() { x++; y++; }
+    int incrX() { return x + 1; }
+    int incrY() { return ++y; }
+};
+
+Identify the set of all methods that change the state of Complex class objects?
+a) setX(), setY(), print()
+b) setX(), setY(), incrX(), incrY()
+c) incr(), incrX(), incrY()
+d) setX(), setY(), incr(), incrY()
+*/
+
+class Complex
+{
+private:
+    int x, y;
+
+public:
+    void setX(int x_) { x = x_; }
+    void setY(int y_) { y = y_; }
+    void print() { cout << x << ", " << y; }
+    void incr()
+    {
+        x++;
+        y++;
+    }
+    int incrX() { return x + 1; }
+    int incrY() { return ++y; }
+};
