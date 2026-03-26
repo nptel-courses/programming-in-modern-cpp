d35039a3c9abfa4842d279316edd9a1a0c53b145
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-26 15:14:33 +0530

    WEEK 03: Assignment 03 - Problem 02         Consider the following code segment.
            #include <iostream>
            using namespace std;
            class Integer {
                int n;
            public:
                Integer() {
                    cout << 0 << " ";
                }
                Integer(int i) : n(i) {
                    cout << n << " ";
                }
            };
            int main() {
                int i = 1;
                Integer n1;               // LINE-1
                Integer *n2 = new Integer(i++);
                Integer *n3;
                new Integer(i++);
                return 0;
            }
    
            What will be the output?
            a) 0 1 0 2
            b) 0 1 2
            c) 0 2 3
            d) 1 2
    
    ```cpp
    #include <iostream>
    using namespace std;
    class Integer
    {
        int n;
    
    public:
        Integer()
        {
            cout << 0 << " ";
        }
        Integer(int i) : n(i)
        {
            cout << n << " ";
        }
    };
    int main()
    {
        int i = 1;
        Integer n1; // LINE-1
        Integer *n2 = new Integer(i++);
        Integer *n3;
        new Integer(i++);
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 03/P02/P02.cpp
@@ -0,0 +1,56 @@
+/*
+Consider the following code segment.
+
+#include <iostream>
+using namespace std;
+class Integer {
+    int n;
+public:
+    Integer() {
+        cout << 0 << " ";
+    }
+    Integer(int i) : n(i) {
+        cout << n << " ";
+    }
+};
+int main() {
+    int i = 1;
+    Integer n1;               // LINE-1
+    Integer *n2 = new Integer(i++);
+    Integer *n3;
+    new Integer(i++);
+    return 0;
+}
+
+What will be the output?
+a) 0 1 0 2
+b) 0 1 2
+c) 0 2 3
+d) 1 2
+*/
+
+#include <iostream>
+using namespace std;
+class Integer
+{
+    int n;
+
+public:
+    Integer()
+    {
+        cout << 0 << " ";
+    }
+    Integer(int i) : n(i)
+    {
+        cout << n << " ";
+    }
+};
+int main()
+{
+    int i = 1;
+    Integer n1; // LINE-1
+    Integer *n2 = new Integer(i++);
+    Integer *n3;
+    new Integer(i++);
+    return 0;
+}
