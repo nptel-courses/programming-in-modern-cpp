6666cd0e953029c94f897c56cb6bab4aeeb72763
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-26 16:24:22 +0530

    WEEK 03: Assignment 03 - Problem 07         Consider the following code segment.
            #include <iostream>
            using namespace std;
            class Counter {
                int a, b, c;
            public:
                Counter(int x = 1) : c(x++), b(x++), a(++x) {}
                void print() { cout << a << ", " << b << ", " << c << endl; }
            };
    
            int main() {
                Counter d(5);
                d.print();
                return 0;
            }
    
            What will be the output?
            a) 6,6,7
            b) 6,5,7
            c) 6,6,5
            d) 7,6,6
    
    ```cpp
    #include <iostream>
    using namespace std;
    class Counter
    {
        int a, b, c;
    
    public:
        Counter(int x = 1) : c(x++), b(x++), a(++x) {}
        void print() { cout << a << ", " << b << ", " << c << endl; }
    };
    
    int main()
    {
        Counter d(5);
        d.print();
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 03/P07/P07.cpp
@@ -0,0 +1,42 @@
+/*
+Consider the following code segment.
+
+#include <iostream>
+using namespace std;
+class Counter {
+    int a, b, c;
+public:
+    Counter(int x = 1) : c(x++), b(x++), a(++x) {}
+    void print() { cout << a << ", " << b << ", " << c << endl; }
+};
+
+int main() {
+    Counter d(5);
+    d.print();
+    return 0;
+}
+
+What will be the output?
+a) 6,6,7
+b) 6,5,7
+c) 6,6,5
+d) 7,6,6
+*/
+
+#include <iostream>
+using namespace std;
+class Counter
+{
+    int a, b, c;
+
+public:
+    Counter(int x = 1) : c(x++), b(x++), a(++x) {}
+    void print() { cout << a << ", " << b << ", " << c << endl; }
+};
+
+int main()
+{
+    Counter d(5);
+    d.print();
+    return 0;
+}
