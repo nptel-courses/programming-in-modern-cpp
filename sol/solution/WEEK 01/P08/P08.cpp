c84e25e2d66d357196592fe9f05de7a192ce7ccd
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-26 14:57:03 +0530

    WEEK 01: Assignment 01 - Problem 08         Consider the following cdoe segment.
            #include <iostream>
            #include <algorithm>
            using namespace std;
            int main() {
                int arr[] = {1, 2, 3, 4, 5};
                for (int i = 0; i < 1; i++) {
                    int j = arr[i];
                    replace(arr, arr + 5, j, *(________________));      //LINE-1
                }
                for (int i = 0; i < 5; ++i) {
                    cout << arr[i] << " ";
                }
                return 0;
            }
    
            Fill in the blank at LINE-1 so that the program generates output as Found.
            a) arr + 4 - i
            b) arr + 5 - i
            c) arr + i - 4
            d) arr + i - 5
    
    ```cpp
    #include <iostream>
    #include <algorithm>
    using namespace std;
    int main()
    {
        int arr[] = {1, 2, 3, 4, 5};
        for (int i = 0; i < 1; i++)
        {
            int j = arr[i];
            replace(arr, arr + 5, j, *(arr + 4 - i)); // LINE-1
        }
        for (int i = 0; i < 5; ++i)
        {
            cout << arr[i] << " ";
        }
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 01/P08/P08.cpp
@@ -0,0 +1,42 @@
+/*
+Consider the following cdoe segment.
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 1; i++) {
+        int j = arr[i];
+        replace(arr, arr + 5, j, *(________________));      //LINE-1
+    }
+    for (int i = 0; i < 5; ++i) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+Fill in the blank at LINE-1 so that the program generates output as Found.
+a) arr + 4 - i
+b) arr + 5 - i
+c) arr + i - 4
+d) arr + i - 5
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 1; i++)
+    {
+        int j = arr[i];
+        replace(arr, arr + 5, j, *(arr + 4 - i)); // LINE-1
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
