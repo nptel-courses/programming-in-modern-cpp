476b7a5575fb12a7c8553ac21f28fbaa156680be
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-26 14:54:53 +0530

    WEEK 01: Assignment 01 - Problem 03         Consider the following code and find the correct output.
            #include<iostream>
            #include<vector>
            #include<algorithm>
            using namespace std;
    
            int main(){
                vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
                rotate(arr.begin(), arr.begin()+2, arr.begin()+4);
                for(auto i : arr)
                    cout << i << " ";
                return 0;
            }
    
            What is the output of the above code?
            a) 3 1 2 4 5 6 7
            b) 3 4 1 2 5 6 7
            c) 4 1 2 3 5 6 7
            d) 1 3 2 4 5 6 7
    
    ```cpp
    #include <iostream>
    #include <vector>
    #include <algorithm>
    using namespace std;
    
    int main()
    {
        vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
        rotate(arr.begin(), arr.begin() + 2, arr.begin() + 4);
        for (auto i : arr)
            cout << i << " ";
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 01/P03/P03.cpp
@@ -0,0 +1,38 @@
+/*
+Consider the following code and find the correct output.
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+    rotate(arr.begin(), arr.begin()+2, arr.begin()+4);
+    for(auto i : arr)
+        cout << i << " ";
+    return 0;
+}
+
+What is the output of the above code?
+a) 3 1 2 4 5 6 7
+b) 3 4 1 2 5 6 7
+c) 4 1 2 3 5 6 7
+d) 1 3 2 4 5 6 7
+*/
+
+/**
+ * option b). 3 4 1 2 5 6 7
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+    rotate(arr.begin(), arr.begin() + 2, arr.begin() + 4);
+    for (auto i : arr)
+        cout << i << " ";
+    return 0;
+}
