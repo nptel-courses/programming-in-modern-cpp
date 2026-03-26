77b86898c360861bf1f58bbc3ed82eab3874fe99
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-03-26 15:02:56 +0530

    WEEK 02: Assignment 02 - Problem 05         Consider the following code segment.
            #include <iostream>
            using namespace std;
    
            void modify(int x1, int &x2, int *x3) { //Line-1
                x2 = x1 * 2;
                *x3 = x1 + 5;
            }
    
            int main() {
                int x = 2, y, z;
                modify(x, y, &z);
                cout << x << " " << y << " " << z;
                return 0;
            }
    
            Choose the correct option for parameter list at LINE-1 such that the output is 2 4 7.
            a) int &x2, int *x3
            b) int *x2, int *x3
            c) int &x2, int &x3
            d) int *x2, int &x3
    
    ```cpp
    #include <iostream>
    using namespace std;
    
    void modify(int x1, int &x2, int *x3)
    { // Line-1
        x2 = x1 * 2;
        *x3 = x1 + 5;
    }
    
    int main()
    {
        int x = 2, y, z;
        modify(x, y, &z);
        cout << x << " " << y << " " << z;
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 02/P05/P05.cpp
@@ -0,0 +1,41 @@
+/*
+Consider the following code segment.
+
+#include <iostream>
+using namespace std;
+
+void modify(int x1, int &x2, int *x3) { //Line-1
+    x2 = x1 * 2;
+    *x3 = x1 + 5;
+}
+
+int main() {
+    int x = 2, y, z;
+    modify(x, y, &z);
+    cout << x << " " << y << " " << z;
+    return 0;
+}
+
+Choose the correct option for parameter list at LINE-1 such that the output is 2 4 7.
+a) int &x2, int *x3
+b) int *x2, int *x3
+c) int &x2, int &x3
+d) int *x2, int &x3
+*/
+
+#include <iostream>
+using namespace std;
+
+void modify(int x1, int &x2, int *x3)
+{ // Line-1
+    x2 = x1 * 2;
+    *x3 = x1 + 5;
+}
+
+int main()
+{
+    int x = 2, y, z;
+    modify(x, y, &z);
+    cout << x << " " << y << " " << z;
+    return 0;
+}
