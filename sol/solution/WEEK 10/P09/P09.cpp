7a4f9492282b9ef75ed3eea5f03870d9fe195c29
Harsh <harsh.github.microsoft@gmail.com>
2026-03-28 15:09:50 +0530

    WEEK 10: Assignment 10 - Problem 09         Consider the code segment (C++11) given below.
            #include <iostream>
            #include <initializer_list>
            using namespace std;
    
            template<typename T>
            class Container {
            public:
                Container() { cout << "ctor-A" << endl; }
                Container(int x) { cout << "ctor-B" << endl; }
                Container(initializer_list<int> il) { cout << "ctor-C" << endl; }
                Container(int x, initializer_list<int> il) {
                    cout << "ctor-D" << endl;
                }
            };
    
            int main(){
                Container<int> c1(5);
                Container<int> c2({1, 2});
                Container<int> c3{1, 2};
                Container<int> c4 = {1, 2};
                Container<int> c5(5, {1, 2});
                return 0;
            }
    
            What will be the output?
    
            a)  ctor-B
                ctor-C
                ctor-C
                ctor-C
                ctor-D
    
            b)  ctor-B
                ctor-C
                ctor-C
                ctor-A
                ctor-D
    
            c)  ctor-B
                ctor-C
                ctor-B
                ctor-A
                ctor-C
    
            d)  ctor-B
                ctor-C
                ctor-C
                ctor-C
                ctor-C
    
    ```cpp
    #include <iostream>
    #include <initializer_list>
    using namespace std;
    
    template <typename T>
    class Container
    {
    public:
        Container() { cout << "ctor-A" << endl; }
        Container(int x) { cout << "ctor-B" << endl; }
        Container(initializer_list<int> il) { cout << "ctor-C" << endl; }
        Container(int x, initializer_list<int> il)
        {
            cout << "ctor-D" << endl;
        }
    };
    
    int main()
    {
        Container<int> c1(5);
        Container<int> c2({1, 2});
        Container<int> c3{1, 2};
        Container<int> c4 = {1, 2};
        Container<int> c5(5, {1, 2});
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 10/P09/P09.cpp
@@ -0,0 +1,80 @@
+/*
+Consider the code segment (C++11) given below.
+
+#include <iostream>
+#include <initializer_list>
+using namespace std;
+
+template<typename T>
+class Container {
+public:
+    Container() { cout << "ctor-A" << endl; }
+    Container(int x) { cout << "ctor-B" << endl; }
+    Container(initializer_list<int> il) { cout << "ctor-C" << endl; }
+    Container(int x, initializer_list<int> il) {
+        cout << "ctor-D" << endl;
+    }
+};
+
+int main(){
+    Container<int> c1(5);
+    Container<int> c2({1, 2});
+    Container<int> c3{1, 2};
+    Container<int> c4 = {1, 2};
+    Container<int> c5(5, {1, 2});
+    return 0;
+}
+
+What will be the output?
+
+a)  ctor-B
+    ctor-C
+    ctor-C
+    ctor-C
+    ctor-D
+
+b)  ctor-B
+    ctor-C
+    ctor-C
+    ctor-A
+    ctor-D
+
+c)  ctor-B
+    ctor-C
+    ctor-B
+    ctor-A
+    ctor-C
+
+d)  ctor-B
+    ctor-C
+    ctor-C
+    ctor-C
+    ctor-C
+*/
+
+#include <iostream>
+#include <initializer_list>
+using namespace std;
+
+template <typename T>
+class Container
+{
+public:
+    Container() { cout << "ctor-A" << endl; }
+    Container(int x) { cout << "ctor-B" << endl; }
+    Container(initializer_list<int> il) { cout << "ctor-C" << endl; }
+    Container(int x, initializer_list<int> il)
+    {
+        cout << "ctor-D" << endl;
+    }
+};
+
+int main()
+{
+    Container<int> c1(5);
+    Container<int> c2({1, 2});
+    Container<int> c3{1, 2};
+    Container<int> c4 = {1, 2};
+    Container<int> c5(5, {1, 2});
+    return 0;
+}
