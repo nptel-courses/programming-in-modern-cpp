5c1415fc6f554dfe55de38eeddde6a8f93b8ed00
Harsh <harsh.github.microsoft@gmail.com>
2026-03-29 12:10:25 +0530

    WEEK 09: Assignment 09 - Problem 05         Consider the following code segment.
            #include <iostream>
            #include <iomanip>
            using namespace std;
    
            int main () {
                cout << fixed << setprecision(4)
                << setfill('*') << setw(12)
                << 25.0/4;
                return 0;
            }
    
            What will be the output?
            a) ******6.2500
            b) ******6.2500
            c) ******6.25
            d) ******6.2500
    
    ```cpp
    #include <iostream>
    #include <iomanip>
    using namespace std;
    
    int main()
    {
        cout << fixed << setprecision(4)
             << setfill('*') << setw(12)
             << 25.0 / 4;
        return 0;
    }
    ```

---
+++ sol/solution/WEEK 09/P05/P05.cpp
@@ -0,0 +1,32 @@
+/*
+Consider the following code segment.
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+    cout << fixed << setprecision(4)
+    << setfill('*') << setw(12)
+    << 25.0/4;
+    return 0;
+}
+
+What will be the output?
+a) ******6.2500
+b) ******6.2500
+c) ******6.25
+d) ******6.2500
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << fixed << setprecision(4)
+         << setfill('*') << setw(12)
+         << 25.0 / 4;
+    return 0;
+}
